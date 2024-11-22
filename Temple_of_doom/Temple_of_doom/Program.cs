a0eb8655a97a2739e78759625436dbfd48d8f44d
Rodin van den Berg <rma.vandenberg@student.avans.nl>
2024-11-22 17:11:04 +0100

    initial

---
+++ Temple_of_doom/Temple_of_doom/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Temple_of_doom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
