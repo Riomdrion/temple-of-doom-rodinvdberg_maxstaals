3b3f8eeaa02f401a4a73565de45b323429b0f020
Rodin van den Berg <rma.vandenberg@student.avans.nl>
2024-12-13 16:37:04 +0100

    player move all by himself

--- Temple_of_doom/TempleOfDoom.data/Models/Map/GameWorld.cs
+++ Temple_of_doom/TempleOfDoom.data/Models/Map/GameWorld.cs
@@ -7,26 +7,5 @@ public class GameWorld
     public Room CurrentRoom { get; set; }
     public bool IsGameOver => Player?.Lives <= 0 || Player?.HasWon == true;
 
-    public void MovePlayer(string direction)
-    {
-        var currentPosition = Player.Position;
-        var newPosition = direction switch
-        {
-            "up" => new Position(currentPosition.X, currentPosition.Y - 1),
-            "down" => new Position(currentPosition.X, currentPosition.Y + 1),
-            "left" => new Position(currentPosition.X - 1, currentPosition.Y),
-            "right" => new Position(currentPosition.X + 1, currentPosition.Y),
-            _ => currentPosition
-        };
-
-        if (CurrentRoom.IsPositionWalkable(newPosition))
-        {
-            Player.Position = newPosition;
-            Console.WriteLine($"Player moved to: {newPosition.X}, {newPosition.Y}");
-        }
-        else
-        {
-            Console.WriteLine("You can't move there!");
-        }
-    }
+    
 }
--- Temple_of_doom/TempleOfDoom.data/Models/Map/Player.cs
+++ Temple_of_doom/TempleOfDoom.data/Models/Map/Player.cs
@@ -2,6 +2,14 @@
 
 public class Player
 {
+    public int Lives { get; set; }
+    public bool HasWon { get; set; }
+    public Position Position { get; set; }
+    public Inventory Inventory { get; set; }
+    public int StartingRoomId { get; set; }
+    public int StartX { get; set; }
+    public int StartY { get; set; }
+    
     public Player(int startX, int startY, int lives)
     {
         StartX = startX;
@@ -12,14 +20,6 @@ public class Player
         Position = new Position(startX, startY);
     }
 
-    public int Lives { get; set; }
-    public bool HasWon { get; set; }
-    public Position Position { get; set; }
-    public Inventory Inventory { get; set; }
-    public int StartingRoomId { get; set; }
-    public int StartX { get; set; }
-    public int StartY { get; set; }
-
     public bool HasKey(string keyColor)
     {
         return Inventory.HasItem(keyColor);
@@ -28,5 +28,23 @@ public class Player
     public Position GetPlayerStartPosition()
     {
         return new Position(StartX, StartY);
+    }
+
+    public void Move(string command, Room currentRoom)
+    {
+        var currentPosition = this.Position;
+        var newPosition = command switch
+        {
+            "up" => new Position(currentPosition.X, currentPosition.Y - 1),
+            "down" => new Position(currentPosition.X, currentPosition.Y + 1),
+            "left" => new Position(currentPosition.X - 1, currentPosition.Y),
+            "right" => new Position(currentPosition.X + 1, currentPosition.Y),
+            _ => currentPosition
+        };
+    
+        if (currentRoom.IsPositionWalkable(newPosition))
+        {
+            this.Position = newPosition;
+        }
     }
 }
