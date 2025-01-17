44ee04cb8d246dd09910a2d1789b38d89ee45b3b
Max Staals <Max.staals@gmail.com>
2025-01-17 17:04:19 +0100

    Small change in symbols

--- Temple_of_doom/TempleOfDoom.data/Enums/Symbols.cs
+++ Temple_of_doom/TempleOfDoom.data/Enums/Symbols.cs
@@ -2,22 +2,21 @@
 {
     public enum Symbols
     {
-    WALL = '#', // Muur
-    START = 'X', // Startpositie van Indy
-    SANKARASTONE = 'S', // Sankara Stone
-    KEY = 'K', // Sleutel met betreffende kleur
-    VERTICALDOOR = '|', // Deur (verticaal)
-    HORIZONTALDOOR = '=', // Deur (horizontaal)
-    PRESSUREPLATE = 'T', // Drukplek
-    TOGGLEDOOR = '⊥', // Deur die met drukplek open/dicht kan
-    CLOSINGGATE = '∩', // Deur die dicht gaat na doorgaan
-    BOOBYTRAP = 'O', // Boobytrap die schade doet
-    DISSAPINGBOOBYTRAP = '@', // Verdwijnt na schade doen
-    NOTHING = ' ',
-    REDVERTICALDOOR = '$',
-    REDHORIZONTALDOOR = '%',
-    GREENVERTICALDOOR = '&',
-    GREENHORIZONTALDOOR = '*',
-
+        WALL = '#',               // Muur
+        INDY = 'X',               // Startpositie van Indy
+        SANKARASTONE = 'S',       // Sankara Stone
+        KEY = 'K',                // Sleutel met betreffende kleur
+        VERTICALDOOR = '|',       // Deur (verticaal)
+        HORIZONTALDOOR = '=',     // Deur (horizontaal)
+        PRESSUREPLATE = 'T',      // Drukplek
+        TOGGLEDOOR = '⊥',         // Deur die met drukplek open/dicht kan
+        CLOSINGGATE = '∩',        // Deur die dicht gaat na doorgaan
+        BOOBYTRAP = 'O',          // Boobytrap die schade doet
+        DISSAPINGBOOBYTRAP = '@', // Verdwijnt na schade doen
+        NODOOR = ' ',             // geen deur aanwezig
+        REDVERTICALDOOR = '$',
+        REDHORIZONTALDOOR = '%',
+        GREENVERTICALDOOR = '&',
+        GREENHORIZONTALDOOR = '*',
     }
 }
