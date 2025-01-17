3efb015f362b8a30b1a3e1af186129f9a0d82450
Max Staals <Max.staals@gmail.com>
2025-01-17 14:50:18 +0100

    Closing gate

--- Temple_of_doom/TempleOfDoom.data/Models/Door/ClosingGate.cs
+++ Temple_of_doom/TempleOfDoom.data/Models/Door/ClosingGate.cs
@@ -5,13 +5,22 @@ namespace TempleOfDoom.data.Models.Door;
 
 public class ClosingGate : DoorDecorator
 {
+    private bool _isActivated;
+    private bool _canOpen;
     public ClosingGate(Door door) : base(door)
     {
         Symbol = (char)Symbols.CLOSINGGATE;
+        _isActivated = false;
+        _canOpen = true;
     }
 
     public override bool CanOpen(Player player)
     {
-        return true; // Always allows the player to pass, but closes after.
+        if (!_isActivated)
+        {
+            _isActivated = true;
+            _canOpen = false;
+        }
+        return _canOpen;
     }
 }
