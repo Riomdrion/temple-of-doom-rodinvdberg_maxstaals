75a1e42143f009522adf4842645c7f31ecd6bc7c
Max Staals <Max.staals@gmail.com>
2025-01-17 15:09:33 +0100

    Changes in doorfactory

--- Temple_of_doom/TempleOfDoom.data/DTO/DoorDTO.cs
+++ Temple_of_doom/TempleOfDoom.data/DTO/DoorDTO.cs
@@ -4,10 +4,7 @@ namespace TempleOfDoom.data.DTO;
 
 public class DoorDto
 {
-    public int Id { get; }
-    public int TargetRoomId { get; }
-    public string Type { get; }
-    public string? KeyColor { get; }
-    public Direction Direction { get; }
-    public int RequiredStones { get; }
+    public string type { get; set; }
+    public string? color { get; set; }
+    public int? no_of_stones { get; set; }
 }
