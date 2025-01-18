de92c732303d3defcb7e7a139516d6a14a056fce
Max Staals <Max.staals@gmail.com>
2025-01-18 16:27:55 +0100

    Begin enemies

---
+++ Temple_of_doom/TempleOfDoom.data/Models/Enemies/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempleOfDoom.data.Models.Enemies
+{
+    public abstract class Enemy
+    {
+        public int X { get; protected set; }
+        public int Y { get; protected set; }
+        public int MinX { get; protected set; }
+        public int MinY { get; protected set; }
+        public int MaxX { get; protected set; }
+        public int MaxY { get; protected set; }
+
+        protected Enemy(int x, int y, int minX, int minY, int maxX, int maxY)
+        {
+            X = x;
+            Y = y;
+            MinX = minX;
+            MinY = minY;
+            MaxX = maxX;
+            MaxY = maxY;
+        }
+
+        public abstract void Move(); // Elke vijand bepaalt zijn eigen beweging
+    }
+}
